6cc0c68b1ba61d1307a7ba2f287355dca44c5b58
Elena Gordienko <elena.gordienko@2bprecisehealth.com>
2025-05-23 07:55:21 -0400

    fix build

--- resume-matching/src/test/java/com/ostj/resumeprocessing/MatcherTest.java
+++ resume-matching/src/test/java/com/ostj/resumeprocessing/MatcherTest.java
@@ -6,8 +6,6 @@ import java.io.InputStream;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 
 public class MatcherTest {
