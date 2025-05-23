d66bfb1d83af6b90a6b656b7b2cc453908d5f41f
Elena Gordienko <elena.gordienko61@gmail.com>
2025-05-23 10:07:24 -0400

    change test

--- resume-matching/src/test/java/com/ostj/resumeprocessing/MatcherTest.java
+++ resume-matching/src/test/java/com/ostj/resumeprocessing/MatcherTest.java
@@ -6,8 +6,6 @@ import java.io.InputStream;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 
 public class MatcherTest {
@@ -15,15 +13,11 @@ public class MatcherTest {
 	
 	@Test
 	public void testGetUserInfo() throws Exception {
-		String resume = readFile(this.getClass().getClassLoader().getResourceAsStream("Person.txt"));
-		String prompt = readFile(this.getClass().getClassLoader().getResourceAsStream("prompt_get_info.txt"));
+		String resume = IOUtils.toString(this.getClass().getClassLoader().getResourceAsStream("Person.txt"),  "UTF-8");
+		String prompt =  IOUtils.toString(this.getClass().getClassLoader().getResourceAsStream("prompt_get_info.txt"),  "UTF-8");
 		String response = matcher.call_openai(resume, "", prompt);
 		System.out.println("Response: " + response);
 		assertTrue(response.contains("Sergei Azarkhin"));
 		assertTrue(response.contains("sergei.azarkhin@gmail.com"));
-	}
-
-	private String readFile(InputStream fis) throws Exception {
-		return IOUtils.toString(fis, "UTF-8");
 	}
 }
