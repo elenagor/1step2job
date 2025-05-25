883f1c13bafc3b1d233d42e74718d3900894d955
Yuri Ackerman <yuriack@yahoo.com>
2025-05-25 17:48:17 -0400

    removed files with password

--- OstjApp/scripts/start_api.sh
+++
@@ -1,8 +0,0 @@
-docker -e ASPNETCORE_HTTP_PORTS=80 \
-    -e ASPNETCORE_ENVIRONMENT=Production \
-    -e ASPNETCORE_ConnectionStrings__DefaultConnection="Host=localhost;Port=5433;Database=ostjdb;Username=ostjsvc;Password=ostjsvc!" \
-    -e ASPNETCORE_Email__Username="yuri.ackerman@1setp2job.ai" \
-    -e ASPNETCORE_Email__Password="oH94E%W$FD*NrxQObB3@TgB@" \
-    -p 80:80 \
-    --name ostjapi \
-    ostjapi
