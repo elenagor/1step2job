85b87f1bdd8bea7ec58e8ee3b81e5dd1bde79863
Yuri Ackerman <yuriack@yahoo.com>
2025-06-08 21:19:09 -0400

    Distinguish completion and embedding endpoints

--- OstjApp/OstjApi/Services/AIClient.cs
+++ OstjApp/OstjApi/Services/AIClient.cs
@@ -8,6 +8,12 @@ namespace OstjApi.Services
 {
     public class AIClientSettings
     {
+        public required AIEndpointSettings Completions { get; set; }
+        public required AIEndpointSettings Embeddings { get; set; }
+    }
+
+    public class AIEndpointSettings
+    {
         public required string ApiUri { get; set; }
         public required string ApiKey { get; set; }
         public required string Model { get; set; }
@@ -15,6 +21,8 @@ namespace OstjApi.Services
 
     public class AIClient : IAIClient
     {
+        private readonly AIEndpointSettings _completionsSettings;
+        private readonly AIEndpointSettings _embeddingSettings;
         private readonly ChatClient _chatClient;
         private readonly EmbeddingClient _embeddingClient;
         private readonly ILogger<AIClient> _logger;
@@ -22,32 +30,32 @@ namespace OstjApi.Services
         public AIClient(IOptions<AIClientSettings> settings, ILogger<AIClient> logger)
         {
             _logger = logger;
+            _completionsSettings = settings.Value.Completions;
+            _embeddingSettings = settings.Value.Embeddings;
 
-            var model = settings.Value.Model;
-            var uri = settings.Value.ApiUri;
-            var apiKey = settings.Value.ApiKey;
-
-            if (string.IsNullOrEmpty(model))
+            if (string.IsNullOrEmpty(_completionsSettings.Model) || string.IsNullOrEmpty(_embeddingSettings.Model))
             {
                 throw new ArgumentNullException("OpenAI.Model", "Model cannot be null or empty.");
             }
 
-            if (string.IsNullOrEmpty(uri))
+            if (string.IsNullOrEmpty(_completionsSettings.ApiUri) || string.IsNullOrEmpty(_embeddingSettings.ApiUri))
             {
                 throw new ArgumentNullException("OpenAI.ApiUri", "Uri cannot be null or empty.");
             }
 
-            if (string.IsNullOrEmpty(apiKey))
+            if (string.IsNullOrEmpty(_completionsSettings.ApiKey) || string.IsNullOrEmpty(_embeddingSettings.ApiKey))
             {
                 throw new ArgumentNullException("OpenAI.ApiKey", "ApiKey cannot be null or empty.");
             }
 
-            OpenAIClientOptions options = new OpenAIClientOptions()
+            _chatClient = new(_completionsSettings.Model, new ApiKeyCredential(_completionsSettings.ApiKey), new OpenAIClientOptions
             {
-                Endpoint = new Uri(uri)
-            };
-            _chatClient = new(model, new ApiKeyCredential(apiKey), options);
-            _embeddingClient = new(model, new ApiKeyCredential(apiKey), options);
+                Endpoint = new Uri(_completionsSettings.ApiUri)
+            });
+            _embeddingClient = new(_embeddingSettings.Model, new ApiKeyCredential(_embeddingSettings.ApiKey), new OpenAIClientOptions
+            {
+                Endpoint = new Uri(_embeddingSettings.ApiUri)
+            });
         }
 
         public async Task<float[]> GenerateEmbeddingAsync(string text)
